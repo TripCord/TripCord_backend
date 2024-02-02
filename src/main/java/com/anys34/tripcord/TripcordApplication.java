fa076711d4de33ed23d7673075f84c7156ba784e
anys34 <anyeseong34@gmail.com>
2024-02-02 16:05:22 +0900

    init

---
+++ src/main/java/com/anys34/tripcord/TripcordApplication.java
@@ -0,0 +1,13 @@
+package com.anys34.tripcord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripcordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripcordApplication.class, args);
+	}
+
+}
