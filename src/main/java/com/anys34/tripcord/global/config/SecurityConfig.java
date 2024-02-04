77f7b1f70717c0d57f28c4fd125fe2734369a1a2
anys34 <anyeseong34@gmail.com>
2024-02-04 19:30:26 +0900

    Feat :: SecurityConfig 수정
    - 모든 요청을 보낼 때 401error가 떠서 모든 요청 허용
    - csrf disable
    
    Ref: #1

--- src/main/java/com/anys34/tripcord/global/config/SecurityConfig.java
+++ src/main/java/com/anys34/tripcord/global/config/SecurityConfig.java
@@ -2,12 +2,25 @@ package com.anys34.tripcord.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.cors().and().
+                authorizeRequests()
+                .anyRequest().permitAll();
+
+        return http.build();
     }
 }
