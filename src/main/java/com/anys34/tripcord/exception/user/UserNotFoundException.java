25259c1da03f236bb40a2d33b006ecc498637399
anys34 <anyeseong34@gmail.com>
2024-02-04 19:34:27 +0900

    Fix :: import 수정
    - 잘못 되어있던 import를 지웠습니다
    Ref: #1

--- src/main/java/com/anys34/tripcord/exception/user/UserNotFoundException.java
+++ src/main/java/com/anys34/tripcord/exception/user/UserNotFoundException.java
@@ -1,7 +1,7 @@
-package com.anys34.youtube.domain.user.exception;
+package com.anys34.tripcord.exception.user;
 
-import com.anys34.youtube.global.config.error.exception.BusinessException;
-import com.anys34.youtube.global.config.error.exception.ErrorCode;
+import com.anys34.tripcord.global.config.error.exception.BusinessException;
+import com.anys34.tripcord.global.config.error.exception.ErrorCode;
 
 public class UserNotFoundException extends BusinessException {
     public static final BusinessException EXCEPTION =
