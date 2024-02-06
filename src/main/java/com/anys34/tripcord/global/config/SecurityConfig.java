5e32b0f5bb12b65d4e36dc845aac87c9c1e9d9fe
anys34 <anyeseong34@gmail.com>
2024-02-06 21:30:45 +0900

    Fix :: PasswordEncoder 수정
    - BCryptPasswordEncoder에서 PasswordEncoder로 변경하였습니다
    
    Ref: #1

--- src/main/java/com/anys34/tripcord/global/config/SecurityConfig.java
+++ src/main/java/com/anys34/tripcord/global/config/SecurityConfig.java
@@ -3,13 +3,16 @@ package com.anys34.tripcord.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
-    public BCryptPasswordEncoder encoder() {
+    public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
--- src/main/java/com/anys34/tripcord/service/user/SignupService.java
+++ src/main/java/com/anys34/tripcord/service/user/SignupService.java
@@ -3,18 +3,20 @@ package com.anys34.tripcord.service.user;
 import com.anys34.tripcord.dto.user.SignupRequest;
 import com.anys34.tripcord.exception.user.UserDuplicateException;
 import com.anys34.tripcord.repository.user.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class SignupService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void execute(SignupRequest signupRequest) {
-        signupRequest.passwordUpdate(bCryptPasswordEncoder.encode(signupRequest.getPassword()));
+        signupRequest.passwordUpdate(passwordEncoder.encode(signupRequest.getPassword()));
 
         duplicateEmail(signupRequest.getEmail());
 
