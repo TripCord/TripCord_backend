404f7d24808973c81f3c331ec85266e6d5dc7d42
anys34 <anyeseong34@gmail.com>
2024-02-14 15:40:15 +0900

    Feat :: principal 추가
    - userDetails와 userDetailsServie를 추가하였습니다.
    
    Ref: #6

---
+++ src/main/java/com/anys34/tripcord/global/security/principle/AuthDetailsService.java
@@ -0,0 +1,18 @@
+package com.anys34.tripcord.global.security.principle;
+
+import com.anys34.tripcord.facade.user.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+    private final UserFacade userFacade;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return new AuthDetails(userFacade.getUserByEmail(email));
+    }
+}
