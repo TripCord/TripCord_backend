49a5f5e9a7d4901093043292f346039d69cce3e8
anys34 <anyeseong34@gmail.com>
2024-02-14 15:47:13 +0900

    Feat :: error code 추가
    - 만료된 JWT를 반환하는 에러 추가
    
    Ref: #6

--- src/main/java/com/anys34/tripcord/global/config/error/exception/ErrorCode.java
+++ src/main/java/com/anys34/tripcord/global/config/error/exception/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
     USER_DUPLICATE(HttpStatus.BAD_REQUEST, "User Duplicate"), // 유저의 이메일이 중복됨
     USER_INCORRECT(HttpStatus.BAD_REQUEST, "User Incorrect"), // 이메일 혹은 비밀번호가 맞지 않음
 
+    EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "Expired Jwt"), // 만료된 JWT
+
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User Not Found"), // 유저를 찾을 수 없음
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
